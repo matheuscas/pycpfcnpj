76f8b35bfc0d42dbcc6ec665a124ab35d4d2578f
matheuscas <matheus.mcas@gmail.com>
2015-02-26 03:59:25 -0300

    updated download url

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
       author='Matheus Cardoso',
       author_email='matheus.mcas@gmail.com',
       url='https://github.com/matheuscas/pycpfcnpj',
-      download_url='',
+      download_url='https://github.com/matheuscas/pycpfcnpj/tarball/1.0',
       keywords=['cnpj','cpj','validation','generation','validacao','geracao'],
       classifiers=[]
 )
