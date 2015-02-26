8ccf0e89745c8c3f418f6540272ba0c9f8cbf70d
matheuscas <matheus.mcas@gmail.com>
2015-02-26 03:52:43 -0300

    setup files to pypi

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+      name='pycpfcnpj',
+      packages=['pycpfcnpj'],
+      version='1.0',
+      description='Python module for brazilian register numbers for persons (CPF) and companies (CNPJ).',
+      author='Matheus Cardoso',
+      author_email='matheus.mcas@gmail.com',
+      url='https://github.com/matheuscas/pycpfcnpj',
+      download_url='',
+      keywords=['cnpj','cpj','validation','generation','validacao','geracao'],
+      classifiers=[]
+)
