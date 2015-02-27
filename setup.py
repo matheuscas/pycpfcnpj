e7d1811bf6c02c390b6f0ff98ea8286f2109011c
matheuscas <matheus.mcas@gmail.com>
2015-02-27 04:12:01 -0300

    setup its back and already functioning

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pycpfcnpj',
+      version='1.0.0',
+      description='Python module for brazilian register numbers for persons (CPF) and companies (CNPJ).',
+      url='https://github.com/matheuscas/pycpfcnpj',
+      author='Matheus Cardoso',
+      author_email='matheus.mcas@gmail.com',
+      license='MIT',
+      packages=['pycpfcnpj'],
+      zip_safe=False)
