0be8083dfc9c36203d3ec778339203cac490db46
Matheus Cardoso <matheus.mcas@gmail.com>
2017-08-05 23:03:11 -0300

    added clear_punctuation to remove punctuation marks from cpf numbers

--- pycpfcnpj/cpf.py
+++ pycpfcnpj/cpf.py
@@ -1,4 +1,5 @@
 from . import calculation as calc
+from . import compatible as compat
 
 
 def validate(cpf_number):
@@ -13,19 +14,21 @@ def validate(cpf_number):
 
     """
 
-    if (len(cpf_number) != 11 or
-       len(set(cpf_number)) == 1):
+    _cpf = compat.clear_punctuation(cpf_number)
+
+    if (len(_cpf) != 11 or
+       len(set(_cpf)) == 1):
         return False
 
     first_cpf_weighs = [10, 9, 8, 7, 6, 5, 4, 3, 2]
     second_cpf_weighs = [11, 10, 9, 8, 7, 6, 5, 4, 3, 2]
-    first_part = cpf_number[:9]
-    first_digit = cpf_number[9]
-    second_digit = cpf_number[10]
+    first_part = _cpf[:9]
+    first_digit = _cpf[9]
+    second_digit = _cpf[10]
 
     if (first_digit == calc.first_check_digit(first_part,
                                               first_cpf_weighs) and
-       second_digit == calc.second_check_digit(cpf_number[:10],
+       second_digit == calc.second_check_digit(_cpf[:10],
                                                second_cpf_weighs)):
         return True
 
--- tests/cpf_tests.py
+++ tests/cpf_tests.py
@@ -7,13 +7,23 @@ class CPFTests(unittest.TestCase):
 
     def setUp(self):
         self.valid_cpf = '11144477735'
+        self.masked_valid_cpf = '111.444.777-35'
         self.invalid_cpf = '11144477736'
+        self.masked_invalid_cpf = '111.444.777-36'
 
     def test_validate_cpf_true(self):
         self.assertTrue(cpf.validate(self.valid_cpf))
+    
+    def test_validate_masked_cpf_true(self):
+        self.assertTrue(cpf.validate(self.masked_valid_cpf))
 
     def test_validate_cpf_false(self):
         self.assertFalse(cpf.validate(self.invalid_cpf))
+    
+    def test_validate_masked_cpf_false(self):
+        self.assertFalse(cpf.validate(self.masked_invalid_cpf))
+    
+    def test_validate_cpf_with_same_numbers(self):
         for i in range(10):
             self.assertFalse(cpf.validate(
                 '{0}'.format(i) * 11
