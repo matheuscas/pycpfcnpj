62c9b9746259a3f904481c0bc2cc562f573c416a
Isabela Pinto <isabelasouzapinto@gmail.com>
2017-10-24 00:22:02 -0300

    [cod] created methods that handles input numbers with punctuation marks and test

--- pycpfcnpj/gen.py
+++ pycpfcnpj/gen.py
@@ -16,3 +16,13 @@ def cnpj():
     while not cnpj_module.validate(cnpj_ramdom):
         cnpj_ramdom = ''.join(random.choice(string.digits) for i in range(14))
     return cnpj_ramdom
+
+
+def cpf_with_punctuation():
+	cpf_ramdom = cpf()
+	return '{}.{}.{}-{}'.format(cpf_ramdom[:3], cpf_ramdom[3:6], cpf_ramdom[6:9], cpf_ramdom[9:])
+
+
+def cnpj_with_punctuation():
+	cnpj_ramdom = cnpj()
+	return '{}.{}.{}/{}-{}'.format(cnpj_ramdom[:2], cnpj_ramdom[2:5], cnpj_ramdom[5:8], cnpj_ramdom[8:12], cnpj_ramdom[12:])
