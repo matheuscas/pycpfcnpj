87e45ced8f21190b687b8b43b5ed26888a3ba975
Henrique <everaldo.lopes@concrete.com.br>
2017-11-03 10:26:02 -0200

    removing six package

--- pycpfcnpj/compatible.py
+++ pycpfcnpj/compatible.py
@@ -1,12 +1,10 @@
+import sys
 import string
-import six
 
 
 def clear_punctuation(document):
     """Remove from document all pontuation signals."""
-    document = str(document)
-
-    if six.PY2:
+    if sys.version_info[0] < 3:
         return document.translate(None, string.punctuation)
     else:
         return document.translate(str.maketrans("", "", string.punctuation))
