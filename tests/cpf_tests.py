3784ca697238baf8b4405ee87eb0c63a74c448db
Henrique <everaldo.lopes@concrete.com.br>
2017-11-03 08:41:01 -0200

    covering another cases

--- tests/cpf_tests.py
+++ tests/cpf_tests.py
@@ -7,7 +7,6 @@ class CPFTests(unittest.TestCase):
 
     def setUp(self):
         self.valid_cpf = '11144477735'
-        self.unicode_valid_cpf = u'11144477735'
         self.masked_valid_cpf = '111.444.777-35'
         self.invalid_cpf = '11144477736'
         self.masked_invalid_cpf = '111.444.777-36'
@@ -31,7 +30,16 @@ class CPFTests(unittest.TestCase):
             ))
 
     def test_validate_cpf_unicode_true(self):
-        self.assertTrue(cpf.validate(self.unicode_valid_cpf))
+        self.assertTrue(cpf.validate(u'11144477735'))
+
+    def test_validate_cpf_unicode_false(self):
+        self.assertFalse(cpf.validate(u'11144477736'))
+
+    def test_validate_masked_unicode_cpf_true(self):
+        self.assertTrue(cpf.validate(u'111.444.777-35'))
+
+    def test_validate_masked_unicode_cpf_false(self):
+        self.assertFalse(cpf.validate(u'111.444.777-38'))
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
