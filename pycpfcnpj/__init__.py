97b8f71f40833065890e979c63f8832c6ecf6015
Matheus Cardoso <matheuscas@Matheuss-MacBook-Pro.local>
2018-02-01 23:22:26 -0300

    updates version to 1.4

--- pycpfcnpj/__init__.py
+++ pycpfcnpj/__init__.py
@@ -1,2 +1,2 @@
 __author__ = 'Matheus Cardoso'
-__version__ = '1.3'
+__version__ = '1.4'
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pycpfcnpj',
-      version='1.3',
+      version='1.4',
       description='Python module for brazilian register numbers for persons (CPF) and companies (CNPJ).',
       classifiers=[
           'Development Status :: 5 - Production/Stable',
