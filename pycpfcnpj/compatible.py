7e11da66cb9127f6d3e6d17f6543c839c4ff1df8
jkwak <jeffersonkr@hotmail.com>
2022-03-23 12:19:40 -0300

    add check_whitespaces decorator

--- pycpfcnpj/compatible.py
+++ pycpfcnpj/compatible.py
@@ -1,6 +1,17 @@
 import re
 
 
+def check_whitespaces(func):
+    def wrapper(document):
+        empty_string = list(filter(lambda x: x == " ", document))
+        if empty_string:
+            raise ValueError("Document cannot have whitespaces.")
+        return func(document)
+
+    return wrapper
+
+
+@check_whitespaces
 def clear_punctuation(document):
     """Remove from document all pontuation signals."""
-    return re.sub(r'\D', '', str(document))
+    return re.sub(r"\D", "", str(document))
