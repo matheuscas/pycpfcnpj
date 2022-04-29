dd1573741f6ca27eed53282e70a716f05105eb58
jkwak <jeffersonkr@hotmail.com>
2022-04-29 19:38:52 -0300

    Add check_special_characters decorator

--- pycpfcnpj/compatible.py
+++ pycpfcnpj/compatible.py
@@ -1,3 +1,11 @@
+def check_special_characters(func):
+    def wrapper(document):
+        not_digit = list(filter(lambda x: not x.isdigit(), clear_punctuation(document)))
+        return False if not_digit else func(document)
+
+    return wrapper
+
+
 def clear_punctuation(document):
     """Remove from document all pontuation signals."""
     return document.translate(str.maketrans({".": None, "-": None, "/": None}))
